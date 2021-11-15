4648961ad8cf45d5207fc09e601e6f7cc0a2bdb4
Tosh Roberts Brockway <tosh@pop-os.localdomain>
2021-11-14 17:53:57 -0800

    Chapter 2 - learning about templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, world!"
+    user = {"username": "tosh"}
+    posts = [
+        {
+            "author": {"username": "squash"},
+            "body": "A rainy day in Portland!"
+        }, 
+        {
+            "author": {"username": "Susan"},
+            "body": "I'm tired from driving all day!"
+        }
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
