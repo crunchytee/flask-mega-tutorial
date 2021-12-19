3bd194b08b5cf0058ab0e05432ce09fc50a0f6e0
crunchytee <tosh.robertsbrockway@gmail.com>
2021-12-19 14:59:19 -0800

    working through chapter 5

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@ from werkzeug.utils import redirect
 from app import app
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
+from app.models import User
+from flask_login import login_user, current_user
 
 
 @app.route("/")
@@ -23,9 +25,14 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(
-            f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}")
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("invalid username or password")
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template("login.html", title="Sign In", form=form)
